2b1fd75d11a5fb2c9be5f8168a2df5a79545a0f5
SPablo2191 <pablosandoval2191@gmail.com>
2022-11-13 17:22:37 -0300

    add: getMovies getMovie

--- index.py
+++ index.py
@@ -1,11 +1,21 @@
-from flask import Flask
+"""CinemarAPI 
+@author: Pablo Sandoval
+"""
 
+from flask import Flask, jsonify
+import requests
 app = Flask(__name__)
+key = '1f54bd990f1cdfb230adb312546d765d'
 
-@app.route('/')
-def index():
-    return 'mil programadores'
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    response = requests.get(f'https://api.themoviedb.org/3/movie/now_playing?api_key={key}&language=en-US')
+    return response.json()
 
-@app.route('/gonza')
-def gonza():
-    return 'SIUUUUUUUUU'
+@app.route('/movies/<int:index>', methods=['GET'])
+def get_movie(index):
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{str(index)}?api_key={key}&language=en-US')
+    return response.json()
+
+
+# app.run(debug=True)
