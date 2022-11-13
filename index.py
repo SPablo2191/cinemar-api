782f1f07dba6e904fe25f18953f5d0066b54c93e
SPablo2191 <pablosandoval2191@gmail.com>
2022-11-13 17:29:10 -0300

    testing

--- index.py
+++ index.py
@@ -2,20 +2,20 @@
 @author: Pablo Sandoval
 """
 
-from flask import Flask, jsonify
-import requests
+from flask import Flask
+# import requests
 app = Flask(__name__)
 key = '1f54bd990f1cdfb230adb312546d765d'
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
-    response = requests.get(f'https://api.themoviedb.org/3/movie/now_playing?api_key={key}&language=en-US')
-    return response.json()
+    # response = requests.get(f'https://api.themoviedb.org/3/movie/now_playing?api_key={key}&language=en-US')
+    return 'response.json()'
 
 @app.route('/movies/<int:index>', methods=['GET'])
 def get_movie(index):
-    response = requests.get(f'https://api.themoviedb.org/3/movie/{str(index)}?api_key={key}&language=en-US')
-    return response.json()
+    # response = requests.get(f'https://api.themoviedb.org/3/movie/{str(index)}?api_key={key}&language=en-US')
+    return 'response.json()'
 
 
 # app.run(debug=True)
