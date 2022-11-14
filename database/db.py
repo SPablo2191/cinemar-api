3799972902de824c28cb1b688abe09e9cb44a436
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-11-14 19:42:03 -0300

    📝 doc methods of db

--- database/db.py
+++ database/db.py
@@ -4,8 +4,14 @@ class db():
         self.connection = sql.connect(db)
         self.cursor = self.connection.cursor()
     def query(self,query):
+        """
+            execute the query in sqlite database
+        """
         self.cursor.execute(query)
     def commit(self):
+        """
+            confirm the query operation
+        """
         self.connection.commit()
     def close(self):
         self.connection.close()
