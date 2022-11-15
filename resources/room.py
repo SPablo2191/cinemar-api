bc3f05af127dfee7171595b1578431d84c7a559e
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-11-15 16:45:31 -0300

    🚑 room array

--- resources/room.py
+++ resources/room.py
@@ -3,11 +3,12 @@ import datetime
 rooms = Blueprint('rooms',__name__)
 @rooms.route('/rooms',methods=['GET'])
 def get_rooms():
-    return jsonify([
-        {'idSala': 1,'nombre':'sala1','cantidadButacas': '10','fechaRegistro':datetime.datetime.now()},
-        {'idSala': 2,'nombre':'sala2','cantidadButacas': '20','fechaRegistro':datetime.datetime.now()},
-        {'idSala': 3,'nombre':'sala3','cantidadButacas': '30','fechaRegistro':datetime.datetime.now()},
-        {'idSala': 4,'nombre':'sala4','cantidadButacas': '40','fechaRegistro':datetime.datetime.now()}])
+    return jsonify([])
+    # return jsonify([
+    #     {'idSala': 1,'nombre':'sala1','cantidadButacas': '10','fechaRegistro':datetime.datetime.now()},
+    #     {'idSala': 2,'nombre':'sala2','cantidadButacas': '20','fechaRegistro':datetime.datetime.now()},
+    #     {'idSala': 3,'nombre':'sala3','cantidadButacas': '30','fechaRegistro':datetime.datetime.now()},
+    #     {'idSala': 4,'nombre':'sala4','cantidadButacas': '40','fechaRegistro':datetime.datetime.now()}])
 @rooms.route('/rooms',methods=['POST'])
 def add_rooms():
     return jsonify([])
