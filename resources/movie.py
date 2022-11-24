f155e426d4a78afd93be97390a69a934112b204a
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-11-24 12:07:58 -0300

    ✨ sending movies only

--- resources/movie.py
+++ resources/movie.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify
 import requests
 movies = Blueprint('movies',__name__)
 key = '1f54bd990f1cdfb230adb312546d765d'
 @movies.route('/movies', methods=['GET'])
 def get_movies():
     response = requests.get(f'https://api.themoviedb.org/3/movie/now_playing?api_key={key}&language=en-US')
-    return response.json()
+    movies = dict(response.json())['results']
+    return jsonify(movies)
 
 @movies.route('/movies/<int:index>', methods=['GET'])
 def get_movie(index):
