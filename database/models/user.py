d824d8d632a01ed3e46c866a00125ccc62369cf1
SPablo2191 <pablosandoval2191@gmail.com>
2023-07-28 14:04:11 -0300

    feat: user model added

---
+++ database/models/user.py
@@ -0,0 +1,15 @@
+from ..db import db
+import datetime
+
+
+class User(db.Document):
+    name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    user_name = db.StringField(required=True, unique=True)
+    DNI = db.StringField(required=True, unique=True)
+    password = db.StringField(required=True)
+    email = db.StringField(required=True, unique=True)
+    phone_number = db.StringField(required=True, unique=True)
+    birth_date = db.DateTimeField()
+    created_at = db.DateTimeField(default=datetime.datetime.utcnow)
+    status = db.BooleanField(default=True)
