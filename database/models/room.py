5772fba21ca01dd198f9b8aa94f72733a1986dd2
SPablo2191 <pablosandoval2191@gmail.com>
2023-07-28 13:26:57 -0300

    feat: room model added

---
+++ database/models/room.py
@@ -0,0 +1,11 @@
+import datetime
+from ..db import db
+
+class Room(db.Document):
+        name = db.StringField(required=True, unique=True)
+        price = db.FloatField(required=True)
+        seats_quantity = db.IntegerField()
+        created_at = db.DateTimeField(default=datetime.datetime.utcnow)
+
+
+
