88d22ffd948c4ae05e9625713feb7b42e4f05c45
SPablo2191 <pablosandoval2191@gmail.com>
2023-07-28 12:16:30 -0300

    fix: index run added

--- index.py
+++ index.py
@@ -5,10 +5,13 @@ from flask import Flask
 from flask_cors import CORS
 from resources.movie import movies
 from resources.room import rooms
+
 app = Flask(__name__)
 CORS(app)
 # Movies routes
 app.register_blueprint(movies)
 # Rooms routes
 app.register_blueprint(rooms)
-# app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
